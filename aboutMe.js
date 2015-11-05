30d49b8cd69edeb4e6eacf3f1a24114bb879e2e7
jazziesf <jazziesf21@yahoo.com>
2015-11-04 18:34:08 -0800

    first commit

---
+++ aboutMe.js
@@ -0,0 +1,41 @@
+
+alert("Welcome to my page");
+
+var userName = "Sebastian";
+
+var question1 = prompt("Does your love for food take you to the most random places?");
+    if (question1.toUpperCase == "y" || question1.toUpperCase == "yes") {
+      alert("The best food experiences are the one's you never plan" + name);
+        }
+    else if (question1.toUpperCase == "n" || question1.toUpperCase == "no") {
+      alert("Be adventures " + userName + " sometimes the best food is found at the hole in the wall down the stree");
+    }
+    else {
+      alert("Youre on the right track, " + userName + " ! Making a food bucket list might encouragment your inner foodie to explore");
+    }
+
+var question2 = prompt("Have you ever been to San Francisco?");
+    if (question2.toUpperCase == "y" || question2.toUpperCase == "yes") {
+      alert(userName + "you get around don't you!");
+      }
+    else if ( question2.toUpperCase == "n" || question2.toUpperCase == "no") {
+      alert(userName + " if you ever find yourself there a must see is Crissy Field");
+     }
+    else {
+      alert(userName + " one times is all it takes to fall in love with San Francisco");
+     }
+
+var question3 = prompt("Based on my images do you think I'm from California");
+    if (question3.toUpperCase == "y" || question3.toUpperCase == "yes") {
+      alert(userName + " they don't call it the golden state for nothing");
+      }
+    else if (question3.toUpperCase == "n" || question3.toUpperCase == "no") {
+      alert(userName + "the Golden Gate Bridge didnt' give it away?");
+      }
+    else {
+      alert(userName + "Washington would have been just a good a guest");
+      }
+
+console.log(question1);
+console.log(question2);
+console.log(question3);
