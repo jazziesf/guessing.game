cb345953e009e8f6479ba94dd58f354c9ff6ca28
jazziesf <jazziesf21@yahoo.com>
2015-11-05 19:37:55 -0800

    add question5 modified strict=

--- aboutMe.js
+++ aboutMe.js
@@ -4,10 +4,11 @@ alert("Welcome to my page");
 var userName = "Sebastian";
 
 var question1 = prompt("Does your love for food take you to the most random places?");
-    if (question1.toUpperCase == "y" || question1.toUpperCase == "yes") {
+    console.log(question1);
+    if (question1.toLowerCase() === "y" || question1.toLowerCase() === "yes") {
       alert("The best food experiences are the one's you never plan " + name);
         }
-    else if (question1.toUpperCase == "n" || question1.toUpperCase == "no") {
+    else if (question1.toLowerCase() === "n" || question1.toLowerCase() === "no") {
       alert("Be adventures " + userName + " sometimes the best food is found at the hole in the wall down the stree");
     }
     else {
@@ -15,10 +16,11 @@ var question1 = prompt("Does your love for food take you to the most random plac
     }
 
 var question2 = prompt("Have you ever been to San Francisco?");
-    if (question2.toUpperCase == "y" || question2.toUpperCase == "yes") {
+    console.log(question2);
+    if (question2.toLowerCase() === "y" || question2.toLowerCase() === "yes") {
       alert(userName + " you get around don't you!");
       }
-    else if ( question2.toUpperCase == "n" || question2.toUpperCase == "no") {
+    else if (question2.toLowerCase() === "n" || question2.toLowerCase() === "no") {
       alert(userName + " if you ever find yourself there a must see is Crissy Field");
      }
     else {
@@ -26,10 +28,11 @@ var question2 = prompt("Have you ever been to San Francisco?");
      }
 
 var question3 = prompt("Based on my images do you think I'm from California");
-    if (question3.toUpperCase == "y" || question3.toUpperCase == "yes") {
+    console.log(question3);
+    if (question3.toLowerCase() === "y" || question3.toLowerCase() === "yes") {
       alert(userName + " they don't call it the golden state for nothing");
       }
-    else if (question3.toUpperCase == "n" || question3.toUpperCase == "no") {
+    else if (question3.toLowerCase() === "n" || question3.toLowerCase() === "no") {
       alert(userName + " the Golden Gate Bridge didnt' give it away?");
       }
     else {
@@ -37,17 +40,27 @@ var question3 = prompt("Based on my images do you think I'm from California");
       }
 
 var question4 = prompt("Do you love corissant?");
-    if (question4.toUpperCase == "y" || question4.toUpperCase == "yes") {
+    console.log(question4);
+    if (question4.toLowerCase() === "y" || question4.toLowerCase() === "yes") {
       alert(userName + " they are savory and sweet all at the same time");
       }
-    else if (question4.toUpperCase == "n" || question4.toUpperCase == "no") {
+    else if (question4.toLowerCase() === "n" || question4.toLowerCase() === "no") {
       alert(userName + " think about what pastery you couldn't live without?");
       }
     else {
       alert(userName + " there isn't a pastery I haven't met I didn't like");
       }
 
-console.log(question1);
-console.log(question2);
-console.log(question3);
-console.log(question4);
+var question5 = prompt("How many times a day do you have coffee");
+    console.log(question5);
+    if (question5 <= 2 ) {
+      alert(userName + " that's pretty miniual for a Seattlite");
+      }
+    else if (question5 >= 3 && question5 <= 7) {
+      alert(userName + " whooo you should slow down");
+      }
+    else {
+      alert(userName + " I'm hope you have medicinal reasons for that much caffine");
+      }
+
+
